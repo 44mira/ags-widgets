1ee629c9062517a82c628c4f05683c5df24fb70f
tyrael <legolaslada@gmail.com>
2024-10-12 03:57:07 +0800

    feat: only display filled workspaces

--- widgets/bar/Workspaces.ts
+++ widgets/bar/Workspaces.ts
@@ -2,25 +2,35 @@ import { range } from "lib/utils";
 
 const hyprland = await Service.import("hyprland");
 
-const Workspaces = () => {
-  const activeWorkspace = hyprland.active.workspace.bind("id");
+const Workspaces = () =>
+  Widget.Box({
+    setup: (self) => {
+      const activeWorkspace = hyprland.active.workspace.bind("id");
 
-  const workspaces = range(9, 1).map((i) =>
-    Widget.Button({
-      label: `${i}`,
-      on_clicked: () => hyprland.messageAsync(`dispatch workspace ${i}`),
-      class_name: activeWorkspace.as(
-        (active) => `${active === i ? "focused-workspace" : ""}`,
-      ),
-    }),
-  );
+      // Limit display to n workspaces
+      const workspaces = range(9, 1).map((i) =>
+        Widget.Button({
+          label: `${i}`,
+          on_clicked: () => hyprland.messageAsync(`dispatch workspace ${i}`),
+          class_name: activeWorkspace.as(
+            (active) => `${active === i ? "focused-workspace" : ""}`,
+          ),
+        }),
+      );
 
-  return Widget.Box({
+      self.children = workspaces;
+
+      // only display filled workspaces
+      self.hook(hyprland, () =>
+        self.children.forEach(
+          (ws) =>
+            // @ts-ignore
+            (ws.visible = hyprland.workspaces.some(({ id }) => id == ws.label)),
+        ),
+      );
+    },
     class_name: "workspaces",
     hpack: "start",
     spacing: 6,
-    children: workspaces,
   });
-};
-
 export default Workspaces;
